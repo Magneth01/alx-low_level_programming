e11778b3c78ac2fe7e8925d3a39ffbc3f914972c
Magneth01 <adexolanrewaju11@gmail.com>
2022-09-10 06:00:09 -0700

    singleNumbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 0 to 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int numbers;
+for (numbers = 0; numbers <= 10; numbers++)
+{
+putchar(numbers);
+}
+putchar('\n');
+return (0);
+}
