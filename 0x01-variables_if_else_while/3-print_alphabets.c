99f05685d36de1ac1fabb0eef75f398ed192cbd1
Magneth01 <adexolanrewaju11@gmail.com>
2022-09-10 05:29:41 -0700

    lowerUpper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,14 @@ int main(void)
 {
 char lower;
 char upper;
-for (lower = 'a'; lower <= 'z'; lower++);
+for (lower = 'a'; lower <= 'z'; lower++)
 {
 putchar(lower);
 }
-putchar('\n')
-for (upper = 'A'; upper <= 'Z'; upper++);
+putchar('\n');
+for (upper = 'A'; upper <= 'Z'; upper++)
 {
-putchar(upper);;
+putchar(upper);
 }
 return (0);
 }
