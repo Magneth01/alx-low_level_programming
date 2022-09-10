e62b27b00b433ace0d1753abbdd89aa42fde6144
Magneth01 <adexolanrewaju11@gmail.com>
2022-09-10 05:48:46 -0700

    except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print lowercase except 'e' and 'q'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char lower;
+for (lower 'a'; lower <= 'z'; lower++)
+{
+if (lower != 'a' && lower != 'q')
+{
+putchar(lower);
+}
+}
+putchar('\n);
+return (0);
+}
