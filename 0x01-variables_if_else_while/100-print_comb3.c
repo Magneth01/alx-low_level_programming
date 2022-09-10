b84358d659ae61b26f3bccfbda6b1cc84f91d907
Magneth01 <adexolanrewaju11@gmail.com>
2022-09-10 09:27:06 -0700

    combi2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,41 +1,5 @@
 #include <stdio.h>
 /**
- * main - print two numbers combi
- *
- * Return: 0
- */
-[1:54 AM, 9/10/2022] +234 706 316 5518: int main(void)
-{
-	int i = '0';
-	int j = '0';
-	while (i <= '9')
-	{
-		while (j <= '9')
-		{
-			if (!(i > j || i == j))
-			{
-				putchar(i);
-				putchar(j);
-				if (i == '8' && j == '9')
-				{
-					putchar('\n');
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			j++;
-		}
-		j = '0';
-		i++;
-	}
-	return (0);
-}
-[1:54 AM, 9/10/2022] +234 706 316 5518: Second advanced
-[1:56 AM, 9/10/2022] +234 706 316 5518: #include <stdio.h>
-/**
  * * main - print numbers from 00 to 99.
  * *
  * * Return: 0 on success
