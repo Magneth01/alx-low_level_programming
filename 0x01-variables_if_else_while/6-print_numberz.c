46c2ce8758d6a8860885c820412c23e0dcb6d0c5
Magneth01 <adexolanrewaju11@gmail.com>
2022-09-10 06:29:22 -0700

    num

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print single number with putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int number;
+for (number = 0; number < 10; number++)
+putchar(number);
+putchar('\n');
+return (0);
+}
