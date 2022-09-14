d16fd279ad21cab4f6100e28afdbb895c9482cb9
Magneth01 <adexolanrewaju11@gmail.com>
2022-09-14 15:58:11 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/** print_alphabet - print lowercase alphabets 
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+	for(c 'a'; c <= z; c++)
+	{
+		_putchar(c);
+	}
+	return (0);
+	_putchar('\n');
+}
+
+
