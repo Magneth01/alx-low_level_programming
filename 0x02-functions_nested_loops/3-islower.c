664bfc1c7112c9c60ff71d2216d225c595801744
Magneth01 <adexolanrewaju11@gmail.com>
2022-09-15 02:28:33 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - determine the case of a letter
+ *
+ * Return: 1
+ */
+void print_alphabet_x10(void)
+{
+	if (c >= 'a'; && c <= 'z')
+	{
+		return (1);
+	}
+	else 
+		return (0);
+}
