494123c275955b96da5cfb319c07dc2f4de6692e
Magneth01 <adexolanrewaju11@gmail.com>
2022-09-15 23:10:21 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * _islower - indicate the case of a letter
+ * _isalpha - function to determine the case of a letter
+ * @c: is the argument of the function isalpha
  *
  * Return: 1
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
