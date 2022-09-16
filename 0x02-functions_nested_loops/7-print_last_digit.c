d13cc02f6aab751d0b59d11921afcb3d6abbfaf4
Magneth01 <adexolanrewaju11@gmail.com>
2022-09-16 02:24:38 -0700

    abs

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,14 +5,14 @@
  *
  * Return: 0
  */
-int print_last_digit(int)
+int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
 	if (last_digit < 0)
 	{
-		_putchar(last_digit * -1)
+		_putchar(last_digit * -1);
 	}
-	_putchar(lastdigit + '0');
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
