86a5595c8c66e6a46e15a7fa11ce9dbe226a2054
Magneth01 <adexolanrewaju11@gmail.com>
2022-09-16 03:04:30 -0700

    jack_bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer - function that prints every minute of the day 
+ * jack_bauer - function that prints every minute of the day
  * n loops from 00:00 to 23:59
  * Return: 0
  */
