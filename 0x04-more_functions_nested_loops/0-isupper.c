93de2b40e507994a2a4a9ac089a49619b14a0244
Magneth01 <adexolanrewaju11@gmail.com>
2022-09-18 15:16:08 -0700

    isUpper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - determine if letter is uppercase
+ * @c is the letter 
+ *
+ * Return: 1 for uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This folder is for Nested-loop project
