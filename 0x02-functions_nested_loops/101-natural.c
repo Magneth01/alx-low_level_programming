8b30de5600970b060d9971414c649b88da7c95dc
Magneth01 <adexolanrewaju11@gmail.com>
2022-09-18 01:18:36 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print all numbers divisible by 3 and 5 below 1024
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+		
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
