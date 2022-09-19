26db1dea47bdfde0e19959c223e5ad1d6cfcfbf7
Magneth01 <adexolanrewaju11@gmail.com>
2022-09-18 18:10:18 -0700

    mostNum

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * print_most_numbers - function that prints the numbers
- * from 0 to 9 followed by a new line.
+ * print_most_numbers - function that prints 0 to 9
  * @n: is the number
- *
- * Return: 0
+ * Return: nothing
  */
+
 void print_most_numbers(void)
 {
 int n;
